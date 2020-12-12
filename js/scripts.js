b2a9452b0c6092f18154ff461c944c8413ff3769
alex999ar <alexandrosaristovoulos@gmail.com>
2020-12-12 16:15:34 +0200

    Seifs about us

--- js/scripts.js
+++ js/scripts.js
@@ -50,3 +50,12 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
 })).catch(err => {
     console.log(err);
 });
+
+//-------------About us-------------//
+$(document).ready(function () {
+  $('.navbar-light .dmenu').hover(function () {
+          $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+      }, function () {
+          $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+      });
+  });
